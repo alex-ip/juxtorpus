e6d98ab89fa73afc13085860cc9d7f6ed16be10c
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2022-08-05 14:37:00 +1000

    refactor(spacy): remove lazy loading for global nlp - possibly confusing syntax

--- juxtorpus/__init__.py
+++ juxtorpus/__init__.py
@@ -2,20 +2,13 @@ import spacy
 from typing import Union
 
 model: str = 'en_core_web_sm'
-_nlp: Union[spacy.Language, None] = None
-
-
-def nlp():
-    global _nlp
-    if _nlp is None:
-        _nlp = spacy.load(model)
-    return _nlp
+nlp: Union[spacy.Language, None] = spacy.load(model)
 
 
 def reload_spacy(model_: str, clear_mem: bool):
-    global model, _nlp
+    global model, nlp
     model = model_
-    _nlp = spacy.load(model)
+    nlp = spacy.load(model)
     if clear_mem:
         import gc
         gc.collect()
