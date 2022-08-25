80224c186ee33df62b081c70e0adcd8c8e314223
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2022-08-25 17:40:48 +1000

    feat(components): move to spaCy v3 custom components - wip

--- juxtorpus/components/__init__.py
+++ juxtorpus/components/__init__.py
@@ -13,66 +13,51 @@ from typing import Dict, Union
 from abc import ABCMeta, abstractmethod
 
 from spacy.tokens.doc import Doc
+from spacy.language import Language
 
 
 class Component(metaclass=ABCMeta):
-    def __init__(self, name):
+    def __init__(self, nlp: Language, name: str):
+        self._nlp = nlp
         self._name = name
-
-    @property
-    def name(self):
-        return self._name
 
     @abstractmethod
     def __call__(self, doc: Doc) -> Doc:
         raise NotImplementedError()
 
-    @classmethod
-    @abstractmethod
-    def cache(cls) -> bool:
-        """ Whether the component is cached. Override this method to determine if it is. """
-        raise NotImplementedError()
-
 
 class ComponentImpl(Component):
 
-    def __init__(self, name: str):
-        super(ComponentImpl, self).__init__(name)
+    def __init__(self, nlp: Language, name: str):
+        super(ComponentImpl, self).__init__(nlp, name)
+        print(name)
 
     def __call__(self, doc: Doc):
-        print(f"Processing doc: {doc}...")
+        print(f"{self.__call__}: Processing doc: {doc}...")
         return doc
 
-    @classmethod
-    def cache(cls) -> bool:
-        return False
+
+@Language.component("stateless_custom_component")
+def stateless_custom_component(doc: Doc):
+    print("I don't do anything but I demonstrate a stateless component implementation.")
+    return doc
+
+
+# EACH FACTORY wrapper represents an INSTANCE of the stateful component.
+@Language.factory(name='stateful_custom_component', default_config={"a_setting": True})
+def stateful_custom_component(nlp: Language, name: str, a_setting):
+    print(name)  # this is the instance name.
+    return ComponentImpl(nlp, name)
 
 
 # NOTE: Code below will need to be abstracted to factories o allow for different inits. No need for this yet I guess.
 # caches for pipeline components with expensive (time or memory) initialisation.
 # key = class name (must be a subclass of Component)
-_cache: Dict[str, Component] = dict()
-
-
-def get_component(name: str) -> Union[Component, None]:
-    """ Returns a pipeline component either from cache or brand new. """
-    if _cache.get(name, None):
-
-        # perf: number of subclasses expected to be small so it shouldn't be a concern for the time being.
-        subcls: Union[abc.ABCMeta, None] = None
-        for _subcls in Component.__subclasses__():
-            if name == _subcls.__name__:
-                subcls = _subcls
-                break
-        if subcls is None: return None
-        if getattr(subcls, 'cache')(subcls):
-            _cache[subcls.__name__] = subcls()  # TODO: this won't allow for different inits!
-            return _cache.get(subcls.__name__)
-        else:
-            return subcls()
 
 
 if __name__ == '__main__':
-    ci = ComponentImpl(name='')
-    print(Component.__subclasses__())
-    print([cls.__name__ for cls in Component.__subclasses__()])
+    import spacy
+
+    nlp = spacy.blank('en')
+    nlp.add_pipe("stateful_custom_component", config={"a_setting": False})  # Notice default setting is True.
+    nlp("This is a sentence.")
