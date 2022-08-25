78e5dbe3f628d6d0c831b8636f17beb15348953c
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2022-08-26 09:59:11 +1000

    feat(components): Add hashtag component - wip

---
+++ juxtorpus/components/hashtags.py
@@ -0,0 +1,51 @@
+from spacy import Language
+from spacy.tokens import Doc
+
+from juxtorpus.components import Component
+
+
+@Language.factory("extract_hashtags")
+def create_hashtag_component(nlp: Language, name: str):
+    return HashtagComponent(nlp, name)
+
+
+class HashtagComponent(Component):
+    def __init__(self, nlp: Language, name: str):
+        super().__init__(nlp, name)
+        if not Doc.has_extension("hashtags"):
+            Doc.set_extension("hashtags", default=[])  # doc._.hashtags is may now be accessed.
+
+        self.matcher = Matcher(nlp.vocab)
+        self.matcher.add("hashtag", patterns=[
+            [{"TEXT": "#"}, {"IS_ASCII": True}]
+        ])
+
+    def __call__(self, doc: Doc) -> Doc:
+        for _, start, end in self.matcher(doc):
+            span = doc[start: end]
+            doc._.hashtags.append(span)
+            return doc
+
+
+if __name__ == '__main__':
+    from juxtorpus import nlp
+    from spacy.matcher import Matcher
+
+    # doing it manually
+    doc = nlp("The #MarchForLife is so very extremely important. To all of you marching --- you have my full support!")
+    patterns = [
+        [{"TEXT": "#"}, {"IS_ASCII": True}]
+    ]
+    m = Matcher(nlp.vocab)
+    m.add("hashtag", patterns)
+
+    print("Using rule based matcher...")
+    matches = m(doc)
+    for _, s, e in matches:
+        span = doc[s: e]
+        print(span)
+
+    # using the custom component.
+    nlp.add_pipe('extract_hashtags')
+    doc = nlp("The #MarchForLife is so very extremely important. To all of you marching --- you have my full support!")
+    print(f"doc._.hashtags: {doc._.hashtags}")
