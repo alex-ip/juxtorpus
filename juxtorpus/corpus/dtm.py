168134a2f52e06a2e482d62aabc79dd011a7973b
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2022-11-03 15:42:46 +1100

    feat(corpus): Add dtm child references root dtm and slices

--- juxtorpus/corpus/dtm.py
+++ juxtorpus/corpus/dtm.py
@@ -11,6 +11,7 @@ from pathlib import Path
 import pandas as pd
 import scipy.sparse
 from typing import Iterable, Union, Generator
+
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import pairwise_distances
 
@@ -24,11 +25,36 @@ It is a component of the corpus class.
 
 
 class DTM(object):
-    def __init__(self, lists_of_wordlist: Iterable[Iterable[str]], root_dtm=None):
+
+    @classmethod
+    def from_wordlists(cls, wordlists: list[list[str]]):
+        return cls().build(wordlists)
+
+    def __init__(self):
+        self.root = self
         self.vectorizer = CountVectorizer()
-        self.dtm = self.vectorizer.fit_transform((' '.join(words) for words in lists_of_wordlist))
-        self.vocab = self.vectorizer.get_feature_names_out()
-        self._term_idx_map = {self.vocab[idx]: idx for idx in range(len(self.vocab))}
+        self._dtm = None
+        self._vocab = None
+        self._term_idx_map = None
+
+        # only used for child dtms
+        self._row_indices = None
+
+    @property
+    def dtm(self):
+        if self.root is self:
+            return self._dtm
+        return self.root._dtm[self._row_indices, :]
+
+    @property
+    def vocab(self):
+        return list(self.root._vocab)
+
+    def build(self, wordlists: list[list[str]]):
+        self._dtm = self.vectorizer.fit_transform((' '.join(words) for words in wordlists))
+        self._vocab = self.vectorizer.get_feature_names_out()
+        self._term_idx_map = {self.vocab[idx]: idx for idx in range(len(self._vocab))}
+        return self
 
     def term_vector(self, terms: Union[str, list[str]]):
         """ Return the term vector represented by the documents. """
@@ -44,25 +70,37 @@ class DTM(object):
         raise NotImplementedError()
 
     def _term_to_idx(self, term: str):
-        if term not in self._term_idx_map.keys(): raise ValueError(f"'{term}' not found in document-term-matrix.")
-        return self._term_idx_map.get(term)
+        if term not in self.root._term_idx_map.keys(): raise ValueError(f"'{term}' not found in document-term-matrix.")
+        return self.root._term_idx_map.get(term)
 
-    # clone by document
-    def cloned(self, doc_indices: list[int]) -> scipy.sparse.csr_matrix:
-        return self.dtm[doc_indices]  # TODO: this should 'probably' return a copy of the DTM object itself.
+    @classmethod
+    def cloned(cls, parent, row_indices: list[int]):
+        cloned = cls()
+        cloned.root = parent.root
+        cloned._row_indices = row_indices
+        return cloned
 
 
 if __name__ == '__main__':
     df = pd.read_csv(Path("./tests/assets/Geolocated_places_climate_with_LGA_and_remoteness_0.csv"))
     corpus = Corpus.from_dataframe(df, col_text='processed_text')
 
-    dtm = DTM(corpus.generate_words())
+    dtm = DTM.from_wordlists(corpus.generate_words())
     print(dtm.term_vector('the').shape)
     print(dtm.term_vector(['the', 'he', 'she']).shape)
 
-    sub_dtm = dtm.cloned([1, 6, 8])
-    print(sub_dtm.shape)
-    assert pairwise_distances(dtm.dtm[1], sub_dtm[0], metric='cosine')[0][0] == 0.0
+    child_dtm = dtm.cloned(dtm, [1, 5, 7, 8])
+    print(child_dtm.term_vector('the').shape)
+    print(child_dtm.term_vector(['the', 'he', 'she']).shape)
 
-# put the DTM in the corpus.
-# TODO: try multiplying with IDF -> TFIDFTransformer
+    # PERF: child dtms slices root dtm each time -- seems negligible since its array access anyway.
+    from timeit import timeit
+
+    start = timeit()
+    for i in range(1000000):
+        dtm.term_vector('the')
+    print(f"Elapsed: {timeit() - start:.100f} s")
+    start = timeit()
+    for i in range(1000000):
+        child_dtm.term_vector('the')
+    print(f"Elapsed: {timeit() - start:.100f} s")
