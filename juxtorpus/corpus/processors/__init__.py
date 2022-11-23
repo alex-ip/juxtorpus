0f49c4b19e82631ddb1aad9d3ae8036c675fe273
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2022-11-23 13:04:01 +1100

    feat(processors): process factory method checks corpus argument

--- juxtorpus/corpus/processors/__init__.py
+++ juxtorpus/corpus/processors/__init__.py
@@ -34,6 +34,7 @@ from .spacy_processor import SpacyProcessor
 
 
 def process(corpus, **kwargs):
+    if not isinstance(corpus, Corpus): raise ValueError(f"corpus is not an instance of {Corpus.__name__}.")
     if 'nlp' in kwargs.keys():
         return SpacyProcessor(kwargs.get('nlp')).run(corpus)
     raise ValueError(f"Only SpacyProcessor is supported at the moment. Please supply nlp argument.")
