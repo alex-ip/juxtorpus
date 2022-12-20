70d9dcda49727a4253e4b9df3c9242dc37de703c
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2022-12-20 17:24:36 +1100

    feat(jux): Add constants - maintain consistency in pandas columns

--- juxtorpus/stats/stats.py
+++ juxtorpus/stats/stats.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 import pandas as pd
 from .loglikelihood_effectsize import log_likelihood_and_effect_size
 from juxtorpus.corpus.freqtable import FreqTable
+from juxtorpus.constants import CORPUS_ID_COL_NAME_FORMAT
 
 if TYPE_CHECKING:
     from juxtorpus import Jux
@@ -21,9 +22,13 @@ class Statistics(object):
             res = res.filter(regex=r'(log_likelihood_llv|bayes_factor_bic|effect_size_ell)')
         else:
             res_list = list()
-            for ft in ftables:
+            for i, ft in enumerate(ftables):
                 res = log_likelihood_and_effect_size([ft, baseline])
                 res = res.filter(regex=r'(log_likelihood_llv|bayes_factor_bic|effect_size_ell)')
+                mapper = dict()
+                for col in res.columns:
+                    mapper[col] = CORPUS_ID_COL_NAME_FORMAT.format(col, i)
+                res.rename(mapper=mapper, axis=1, inplace=True)
                 res_list.append(res)
             res = pd.concat(res_list, axis=1)
         # reformat to be consistent with jux
