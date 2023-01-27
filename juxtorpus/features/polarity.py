a108428d32b272d49120c6f1d91bd15a62917831
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-01-27 15:46:33 +1100

    doc(jux): Add polarity docs

--- juxtorpus/features/polarity.py
+++ juxtorpus/features/polarity.py
@@ -14,36 +14,61 @@ Output:
 from typing import TYPE_CHECKING
 import weakref as wr
 import pandas as pd
+from typing import Generator
+from juxtorpus.corpus.dtm import DTM
 
 if TYPE_CHECKING:
     from juxtorpus import Jux
 
 
-# output dataframe:
-# score A, score B, polarity
 class Polarity(object):
+    """ Polarity
+    Gives a 'polarity' score of the two corpus based on token statistics.
+    A polarity score uses 0 as the midline, a positive score means corpus 0 is dominant. And vice versa.
+    e.g. tf would use the term frequencies to use as the polarity score.
+
+    Each function will return a dataframe with a 'polarity' column and other columns with values that composes
+    the 'polarity' score.
+    """
+
     def __init__(self, jux: 'Jux'):
         self._jux: wr.ref['Jux'] = wr.ref(jux)
 
-    def tf(self):
-        """ Term frequency polarity is given by """
-        fts = (corpus.dtm.freq_table() for corpus in self._jux().corpora)
+    def tf(self, tokeniser_func: Optional = None):
+        """ Uses the term frequency to produce the polarity score.
+
+        Polarity = Corpus 0's tf - Corpus 1's tf.
+        """
+        dtms = self._selected_dtms(tokeniser_func)
+        fts = (dtm.freq_table() for dtm in dtms)
 
         renamed_ft = [(f"{ft.name}_corpus_{i}", ft) for i, ft in enumerate(fts)]
         df = pd.concat([ft.series.rename(name) / ft.total for name, ft in renamed_ft], axis=1).fillna(0)
         df['polarity'] = df[renamed_ft[0][0]] - df[renamed_ft[1][0]]
         return df
 
-    def tfidf(self):
-        fts = (corpus.dtm.tfidf().freq_table() for corpus in self._jux().corpora)
+    def tfidf(self, tokeniser_func: Optional = None):
+        """ Uses the tfidf scores to produce the polarity score.
+
+        Polarity = Corpus 0's tfidf - Corpus 1's tfidf.
+        """
+        dtms = self._selected_dtms(tokeniser_func)
+        fts = (dtm.freq_table() for dtm in dtms)
         renamed_ft = [(f"{ft.name}_corpus_{i}", ft) for i, ft in enumerate(fts)]
         df = pd.concat([ft.series.rename(name) / ft.total for name, ft in renamed_ft], axis=1).fillna(0)
         df['polarity'] = df[renamed_ft[0][0]] - df[renamed_ft[1][0]]
         return df
 
-    def log_likelihood(self):
+    def log_likelihood(self, tokeniser_func: Optional = None):
         j = self._jux()
         llv = j.stats.log_likelihood_and_effect_size()
-        tf_polarity = self.tf()['polarity']
+        tf_polarity = self.tf(tokeniser_func)['polarity']
         llv['polarity'] = (tf_polarity * llv['log_likelihood_llv']) / tf_polarity.abs()
         return llv
+
+    def _selected_dtms(self, tokeniser_func: Optional) -> Generator[DTM, None, None]:
+        """ Return a generator DTMs given a tokeniser function."""
+        if tokeniser_func:
+            return (corpus.create_custom_dtm(tokeniser_func) for corpus in self._jux().corpora)
+        else:
+            return (corpus.dtm for corpus in self._jux().corpora)
