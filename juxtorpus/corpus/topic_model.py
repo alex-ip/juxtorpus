036b99329446b49c27d72c834a9e06725bb17f85
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-03-07 12:10:51 +1100

    feat(topicmodel): lda - Add add_to_corpus() - adds as meta

--- juxtorpus/corpus/topic_model.py
+++ juxtorpus/corpus/topic_model.py
@@ -11,13 +11,16 @@ corpus.topic_model.lda.add_as_meta(id_)
 import weakref as wr
 from typing import Callable
 
+import pyLDAvis
 from pyLDAvis import prepare
 from pyLDAvis.sklearn import _row_norm
 from nltk.corpus import stopwords
 from sklearn.decomposition import LatentDirichletAllocation
+import pandas as pd
 
 from juxtorpus.corpus import Corpus
 from juxtorpus.viz import Widget
+from juxtorpus.corpus.meta import SeriesMeta
 
 
 class LDA(Widget):
@@ -25,34 +28,52 @@ class LDA(Widget):
     def __init__(self, corpus: Corpus, num_topics: int):
         self._corpus = wr.ref(corpus)
         self._model = LatentDirichletAllocation(n_components=num_topics, random_state=42)
+        self._topics = None
+        self._pyldavis_args = None
+        self._is_built = False
 
     @property
     def model(self):
         return self._model
 
-    def _build(self, mode: str):
+    def build(self, mode: str):
         assert mode in {'tf', 'tfidf'}, "Only supports mode tf or tfidf."
         if mode == 'tf':
             with self._corpus().dtm.without_terms(terms=list(stopwords.words('english'))) as dtm_nosw:
-                self._model.fit(dtm_nosw.matrix)
+                self._topics = self._model.fit_transform(dtm_nosw.matrix)
                 args = self._get_pyLDAvis_prepare_args(dtm_nosw)
-                return prepare(**args)
+                self._pyldavis_args = args
         if mode == 'tfidf':
             dtm_tfidf = self._corpus().dtm.tfidf()
-            self._model.fit(dtm_tfidf.matrix)
+            self._topics = self._model.fit_transform(dtm_tfidf.matrix)
             args = self._get_pyLDAvis_prepare_args(dtm_tfidf)
-            return prepare(**args)
+            self._pyldavis_args = args
+        self._is_built = True
+        return self
 
     def widget(self):
-        return self._build('tf')
+        if not self._is_built: raise ValueError(f"You haven't built the model yet. Call {self.build.__name__}")
+        pyLDAvis.enable_notebook()
+        return prepare(**self._pyldavis_args)
+
+    def add_to_corpus(self):
+        meta = SeriesMeta(id_='#lda_topic', series=self._get_best_topic_series())
+        self._corpus().update_meta(meta)
+        meta = SeriesMeta(id_='#lda_topics_scores', series=self._get_topics_dataframe())
+        self._corpus().update_meta(meta)
+
+    def _get_topics_dataframe(self):
+        return pd.DataFrame(self._topics, columns=[f"#lda_topic_{i}" for i in range(self._topics.shape[1])])
+
+    def _get_best_topic_series(self):
+        return pd.Series(self._topics.argmax(axis=1))
 
     def _get_pyLDAvis_prepare_args(self, dtm) -> dict:
         vocab = dtm.term_names
         doc_lengths = dtm.total_docs_vector
         term_freqs = dtm.total_terms_vector
         topic_term_dists = _row_norm(self._model.components_)
-        doc_topic_dists = self._model.transform(dtm.matrix)
-
+        doc_topic_dists = self._topics
         return {
             'vocab': vocab,
             'doc_lengths': doc_lengths.tolist(),
