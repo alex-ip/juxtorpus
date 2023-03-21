e6ab779d58a89b45e751fd24d148867c8f9dc06d
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-03-21 13:04:46 +1100

    test(corpus): spacy - Add custom_dtm tests, detach and root

--- tests/test_spacy_corpus.py
+++ tests/test_spacy_corpus.py
@@ -27,6 +27,18 @@ class TestSpacyCorpus(TestCase):
         for doc in self.scorpus.docs():
             assert isinstance(doc, Doc), "SpacyCorpus must contain spacy documents."
 
+    def test_Given_scorpus_When_cloned_Then_cloned_parent_is_scorpus(self):
+        """ Ensure that the parent of clone is the corpus it is cloned from. """
+        mask, _ = random_mask(self.scorpus)
+        clone = self.scorpus.cloned(mask)
+        assert clone.parent == self.scorpus, "Cloned parent is not inherited properly."
+
+    def test_Given_cloned_When_detached_Then_cloned_is_root(self):
+        mask, _ = random_mask(self.scorpus)
+        clone = self.scorpus.cloned(mask)
+        clone.detached()
+        assert clone.is_root, "When detached, the clone should now be the root corpus."
+
     def test_Given_scorpus_When_cloned_Then_correct_docs_are_cloned(self):
         mask, num_trues = random_mask(self.scorpus)
         clone = self.scorpus.cloned(mask)
@@ -72,16 +84,35 @@ class TestSpacyCorpus(TestCase):
             original_idx = docs.index[clone_idx]
             assert is_equal(self.scorpus.dtm.matrix[original_idx, :], clone_again.dtm.matrix[clone_idx, :])
 
-    def test_Given_scorpus_When_cloned_Then_cloned_custom_dtm_is_valid(self):
-        pass
+    def test_Given_clone_When_create_custom_dtm_Then_clone_is_detached(self):
+        mask, _ = random_mask(self.scorpus)
+        clone = self.scorpus.cloned(mask)
+        _ = clone.create_custom_dtm(tokeniser_func=lambda doc: [t.text.lower() for t in doc])
+        assert clone.is_root, "Clone should be root corpus after creation of custom dtm."
 
-    def test_Given_scorpus_When_cloned_Then_cloned_parent_is_scorpus(self):
-        """ Ensure that the parent of clone is the corpus it is cloned from. """
-        pass
+    def test_Given_scorpus_When_cloned_Then_cloned_custom_dtm_is_valid(self):
+        _ = self.scorpus.create_custom_dtm(tokeniser_func=lambda doc: [t.text.lower() for t in doc])
+        mask, _ = random_mask(self.scorpus)
+        clone = self.scorpus.cloned(mask)
+        docs = clone.docs()
+        clone_indices = np.random.randint(0, len(docs), size=5)
+        for clone_idx in clone_indices:
+            original_idx = docs.index[clone_idx]
+            assert is_equal(self.scorpus.custom_dtm.matrix[original_idx, :], clone.custom_dtm.matrix[clone_idx, :])
+
+        mask, num_trues = random_mask(clone)
+        clone_again = clone.cloned(mask)
+        docs = clone_again.docs()
+        clone_indices = np.random.randint(0, len(docs), size=5)
+        for clone_idx in clone_indices:
+            original_idx = docs.index[clone_idx]
+            assert is_equal(self.scorpus.custom_dtm.matrix[original_idx, :],
+                            clone_again.custom_dtm.matrix[clone_idx, :])
 
     def test_Given_scorpus_When_cloned_twice_Then_final_clone_root_is_scorpus(self):
         """ Ensure that the root of the second clone is the root corpus. """
-        pass
-
-    def test_Given_cloned_When_detached_Then_cloned_is_root(self):
-        pass
+        num_clones = 2
+        for i in range(num_clones):
+            mask, _ = random_mask(self.scorpus)
+            clone = self.scorpus.cloned(mask)
+            assert clone.find_root() == self.scorpus, "Clone's root is not the correct root corpus."
