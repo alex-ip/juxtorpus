dbee51070dc63939a2dec2f163ec538b4e85daea
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-04-04 14:16:28 +1000

    feat(corpus): Add Corpora import to __init__

--- juxtorpus/corpus/__init__.py
+++ juxtorpus/corpus/__init__.py
@@ -1,5 +1,6 @@
 
 # import aliases
 from .corpus import Corpus, SpacyCorpus
+from .corpora import Corpora
 from .builder import CorpusBuilder
 from .slicer import CorpusSlicer, SpacyCorpusSlicer, slicer
