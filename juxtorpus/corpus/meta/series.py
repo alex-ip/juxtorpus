67d254eaac9ba1cf02a80f61c8437c1a73148977
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-04-06 10:56:45 +1000

    feat(meta): series - Add dtype @property

--- juxtorpus/corpus/meta/series.py
+++ juxtorpus/corpus/meta/series.py
@@ -25,6 +25,10 @@ class SeriesMeta(Meta):
             self._series = self._series.load()
         return self._series
 
+    @property
+    def dtype(self):
+        return self.series.dtype
+
     def apply(self, func):
         return self.series.apply(func)
 
